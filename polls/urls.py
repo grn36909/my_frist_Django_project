25f0056e84e7a989320d11a5c3d8b056a134716b
grn36909 <grn36909@gmail.com>
2018-09-27 16:22:07 +0800

    creat branch "Step01" 准备进行优化操作

--- polls/urls.py
+++ polls/urls.py
@@ -16,10 +16,10 @@ urlpatterns = [
     re_path(r'^hello/', views.HelloView.as_view(), name='hello'),
 
     # exp: /polls/1/
-    re_path(r'^(?P<question_id>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
     # exp: /polls/1/results/
-    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.ResultsView.as_view, name='results'),
+    re_path(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view, name='results'),
 
     # exp: /polls/1/vote/
     re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
